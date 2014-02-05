b54b0b19ad2692f9f0ab1a30500c1ff9c355ccff
Ag <github@assembledgoods.com>
2014-02-04 20:33:58 -0600

    fix route

--- app/overrides/admin.rb
+++ app/overrides/admin.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/_configuration_menu",
                     :name => "converted_admin_configurations_menu_394888068",
                     :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
-                    :text => %q{<%= configurations_sidebar_menu_item I18n.t('contact_topics'), admin_contact_topics_path %>},
+                    :text => %q{<%= configurations_sidebar_menu_item I18n.t('contact_topics'), spree.admin_contact_topics_path %>},
                     :disabled => false)
